a7a94e9bea7804b7db927e7af41f95e888e03c19
Tom <754275141@.com>
2017-12-15 07:56:01 +0800

    第一次

---
+++ js/main.js
@@ -0,0 +1,74 @@
+/**
+ * Created by lenovo on 17/12/10 010.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop:true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
+
+var music=document.querySelector(".music");
+var musicimg=document.querySelector(".musicimg");
+var audio=document.querySelector("audio");
+var s=1;
+ music.onclick=function(){
+     if (s==1){
+         musicimg.style.animation="none";
+         audio.pause();
+         s=2
+     }else {
+         musicimg.style.animation=" xuanzhuan 2s linear infinite";
+         audio.play();
+         s=1
+     }
+
+ };
+
+//创建和初始化地图函数：
+function initMap(){
+    createMap();//创建地图
+    setMapEvent();//设置地图事件
+    addMapControl();//向地图添加控件
+    addMapOverlay();//向地图添加覆盖物
+}
+function createMap(){
+    map = new BMap.Map("map");
+    map.centerAndZoom(new BMap.Point(116.5776,38.082711),15);
+}
+function setMapEvent(){
+    map.enableScrollWheelZoom();
+    map.enableKeyboard();
+    map.enableDragging();
+    map.enableDoubleClickZoom()
+}
+function addClickHandler(target,window){
+    target.addEventListener("click",function(){
+        target.openInfoWindow(window);
+    });
+}
+function addMapOverlay(){
+}
+//向地图添加控件
+function addMapControl(){
+    var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
+    scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
+    map.addControl(scaleControl);
+    var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+    map.addControl(navControl);
+    var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
+    map.addControl(overviewControl);
+}
+var map;
+initMap();
